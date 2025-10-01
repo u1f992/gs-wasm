f9d76628265601f225d5847bdfaa6649993891d9
Koutaro Mukai <mukai.k1011k@outlook.jp>
2025-10-01 13:14:35 +0000

    patch

--- patch.js
+++ patch.js
@@ -5,40 +5,6 @@ import path from "node:path";
 import url from "node:url";
 
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
-// // Use context mutex to provide thread-safe time
-// cmsBool _cmsGetTime(struct tm* ptr_time)
-// {
-// #ifdef __EMSCRIPTEN__
-//     if (ptr_time) {
-//         memset(ptr_time, 0, sizeof(struct tm));  // 安全な初期化
-//     }
-//     return TRUE;
-// #else
-//     struct tm* t;
-// #if defined(HAVE_GMTIME_R) || defined(HAVE__GMTIME64_S)
-//     struct tm tm;
-// #endif
-
-//     time_t now = time(NULL);
-
-// #ifdef HAVE_GMTIME_R
-//     t = gmtime_r(&now, &tm);
-// #elif defined(HAVE__GMTIME64_S)
-//     t = _gmtime64_s(&tm, &now) == 0 ? &tm : NULL;
-// #else
-//     _cmsEnterCriticalSectionPrimitive(&_cmsContextPoolHeadMutex);
-//     t = gmtime(&now);
-//     _cmsLeaveCriticalSectionPrimitive(&_cmsContextPoolHeadMutex);
-// #endif
-
-//     if (t == NULL)
-//         return FALSE;
-//     else {
-//         *ptr_time = *t;
-//         return TRUE;
-//     }
-// #endif
-// }
 {
   /*
    * To support WebAssembly builds, we copy the existing
@@ -57,4 +23,3 @@ const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
   );
   fs.copyFileSync(src, dest);
 }
-
