0585331a5fbbddd86a52d1ab61446f3ad37a682a
Koutaro Mukai <mukai.k1011k@outlook.jp>
2025-10-03 13:33:45 +0900

    Add CLI interface for gs-wasm

---
+++ src/cli.ts
@@ -0,0 +1,44 @@
+import { gs } from "./index.js";
+
+process.exit(
+  (
+    await gs({
+      args: process.argv.slice(2),
+      onStdin: (() => {
+        const stdinBuffer: (number | null)[] = [];
+        const waitingResolvers: ((value: number | null) => void)[] = [];
+        function wake() {
+          while (waitingResolvers.length > 0 && stdinBuffer.length > 0) {
+            waitingResolvers.shift()!(stdinBuffer.shift()!);
+          }
+        }
+        process.stdin.on("data", (data) => {
+          stdinBuffer.push(...data, null);
+          wake();
+        });
+        process.stdin.on("close", () => {
+          stdinBuffer.push(null);
+          wake();
+        });
+        return async () =>
+          new Promise((resolve) => {
+            if (stdinBuffer.length > 0) {
+              resolve(stdinBuffer.shift()!);
+            } else {
+              waitingResolvers.push(resolve);
+            }
+          });
+      })(),
+      onStdout(charCode) {
+        if (charCode !== null) {
+          process.stdout.write(String.fromCharCode(charCode));
+        }
+      },
+      onStderr(charCode) {
+        if (charCode !== null) {
+          process.stderr.write(String.fromCharCode(charCode));
+        }
+      },
+    })
+  ).exitCode,
+);
